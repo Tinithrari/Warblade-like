cabf3f8594705651c6bd83af9083241f734a3bc4
Tinithrari <xavier.heugue@gmail.com>
2014-11-16 22:45:23 +0100

    Pour que le boss soit plus résistant

--- spaceinvaders/src/entities/enemyEntities/BlackShip.java
+++ spaceinvaders/src/entities/enemyEntities/BlackShip.java
@@ -23,7 +23,7 @@ public class BlackShip extends EnemyEntity {
 		player.setVolume(50.0f);
 		this.gameScene = gameScene;
 		this.attack = attack;
-		this.setlP(400);
+		this.setlP(1800);
 	}
 
 	@Override
